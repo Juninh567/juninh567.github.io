cd0aa4a41ad37986fe9440e5854a4c6c34b147db
Juninho <91491363+Juninh567@users.noreply.github.com>
2022-07-22 18:08:48 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,33 @@
+
+
+  // Import the functions you need from the SDKs you need
+
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.1/firebase-app.js";
+
+  // TODO: Add SDKs for Firebase products that you want to use
+
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+
+  // Your web app's Firebase configuration
+
+  const firebaseConfig = {
+
+    apiKey: "AIzaSyDs0ISJ0AnzI7Saqvq_xbMHvj42iAlDBqs",
+
+    authDomain: "xhgsaaa.firebaseapp.com",
+
+    projectId: "xhgsaaa",
+
+    storageBucket: "xhgsaaa.appspot.com",
+
+    messagingSenderId: "421328396948",
+
+    appId: "1:421328396948:web:7a782a97f47c17b1e08a81"
+
+  };
+
+
+  // Initialize Firebase
+
+  const app = initializeApp(firebaseConfig);
